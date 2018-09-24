47ec2c2873352093cc1a86dc2125f983ec5611b8
johndev1003 <johndev1003@outlook.jp>
2018-09-24 19:10:18 -0400

    :construction: Parse initial user data from support json

--- commands/parse.js
+++ commands/parse.js
@@ -1,6 +1,7 @@
 const BaseCommand = require('../BaseCommand.js');
 const path = require('path');
 const fs = require('fs');
+const MagiRecoUser = require('../model/MagiRecoUser').MagiRecoUser;
 
 module.exports = class Parse extends BaseCommand {
     constructor(debug=false) {
@@ -28,8 +29,45 @@ module.exports = class Parse extends BaseCommand {
             return;
         }
 
+        // Get user_id // id from friendSearchData
+        // and send request to Supports with user_id
+
 
         message.channel.send(JSON.stringify(this.data));
+
+
+        let supportsPath = path.normalize(`${this._basePath}/data/supports.json`);
+        this.supportsData = (fs.existsSync(friendDataPath) && JSON.parse(fs.readFileSync(supportsPath))) || {};
+        //console.log(this.supportsData);
+
+        if ("interrupt" in this.supportsData || !("supportUserList" in this.supportsData)) {
+            message.channel.send(`ERROR: The query failed: ${JSON.stringify(this.supportsData)}`);
+            return;
+        }
+        if (this.supportsData.supportUserList.length == 0) {
+            message.channel.send("No results");
+            // Check if current data, notify users if nothing (typo suspected)
+            return;
+        }
+
+        var users = [];
+        var user = new MagiRecoUser();
+        for (var supportUserIndex in this.supportsData.supportUserList) {
+            var supportUser = this.supportsData.supportUserList[supportUserIndex];
+            console.log(supportUser);
+            user.user_id = supportUser.userId;
+            user.friend_id = supportUser.inviteCode;
+            user.display_name = supportUser.userName;
+            user.user_rank = supportUser.userRank;
+            user.class_rank = supportUser.definitiveClassRank;
+            user.last_access = supportUser.lastAccessDate;
+            user.comment = supportUser.comment;
+            users.push(user);
+            user = new MagiRecoUser();
+        }
+
+        console.log(users);
+        message.channel.send(JSON.stringify(users));
 
         return;
     }
