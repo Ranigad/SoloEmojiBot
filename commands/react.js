ebf5bcbed690ee9cd27299e3057c3ad5a63c1198
johndev1003 <johndev1003@outlook.jp>
2018-10-15 20:31:24 -0400

    Redesign react command using await logic

--- commands/react.js
+++ commands/react.js
@@ -1,8 +1,9 @@
 const BaseCommand = require('../BaseCommand.js');
 
 module.exports = class React extends BaseCommand {
-    constructor(debug=false) {
+    constructor(debug=false, config=undefined) {
         super(debug);
+        this.config = config;
     }
 
     handler(...args) {
@@ -21,7 +22,27 @@ module.exports = class React extends BaseCommand {
         this.run(message_id, emoji, message, bot.emojis);
     }
 
-    run(message_id, emoji_name, message, emojis) {
+    async react_to_message(message, emoji, mainmsg) {
+        try {
+            let reaction = await message.react(emoji);
+            if (mainmsg.deletable) mainmsg.delete();
+
+            setTimeout(function() {
+                reaction.remove();
+            }, 20000);
+            return true;
+        }
+        catch(ex) {
+            mainmsg.channel.send(`Could not apply the given emote.`)
+            .then(message => {
+                message.delete(10000)
+            });
+            if (mainmsg.deletable) mainmsg.delete(10000)
+            return false;
+        }
+    }
+
+    async run(message_id, emoji_name, message, emojis) {
         let emoji = emojis.find('name', emoji_name);
 
         let mainmsg = message;
@@ -37,39 +58,31 @@ module.exports = class React extends BaseCommand {
         }
         else channels.push(message.channel);
 
-        react_to_message(channels);
-
-        function react_to_message(channels) {
+        let success = false;
+        while (channels.length > 0) {
             let channel = channels.shift();
-            if (channel == undefined) {
-                mainmsg.channel.send('Could not find the given message.')
-                .then(message => {
-                    message.delete(10000)
-                });
-                if (mainmsg.deletable) mainmsg.delete(10000);
-            }
-            channel.fetchMessage(message_id)
-            .then(message => {
-                message.react(emoji)
-                    .then((reaction) => {
-                        if (mainmsg.deletable) mainmsg.delete();
+            if (channel == undefined || channel.lastMessageID == undefined) continue;
+            try {
+                let message = await channel.fetchMessage(message_id);
 
-                        setTimeout(function() {
-                            reaction.remove();
-                        }, 20000)
-                    })
-                    .catch(() => {
-                        mainmsg.channel.send(`Could not apply the given emote.`)
-                        .then(message => {
-                            message.delete(10000)
-                        });
-                        if (mainmsg.deletable) mainmsg.delete(10000)
-                    });
-            })
-            .catch(() =>{
-                react_to_message(channels);
-            });
+                let applied = await this.react_to_message(message, emoji, mainmsg);
+
+                if (applied == false) return;
+                else success = true;
+            }
+            catch(ex) {
+                continue;
+            }
         }
+
+        if (success == false) {
+            mainmsg.channel.send('Could not find the given message.')
+            .then(message => {
+                message.delete(10000)
+            });
+            if (mainmsg.deletable) mainmsg.delete(10000);
+        }
+
     }
 
 }
